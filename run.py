50851c18c57ff86b4897703460be675fb74f46aa
EmmaNieves <30022ema@gmail.com>
2026-06-17 13:59:38 -0500

    Update comment for app.py execution

--- run.py
+++ run.py
@@ -2,5 +2,5 @@ import sys, os
 HERE = os.path.dirname(os.path.abspath(__file__))
 sys.path.insert(0, HERE)
 os.chdir(HERE)
-# Arranca la app.py (ya tiene auto-instalador interno)
+# Arranca la app.py
 exec(open(os.path.join(HERE, "app.py")).read())
